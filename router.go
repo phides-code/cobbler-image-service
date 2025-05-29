e6faab8d07d061b20be1f1fc9483c1b6d0641763
Philippe DeSousa <phil.desousa@gmail.com>
2025-05-29 19:58:04 -0400

    update allow-headers

--- router.go
+++ router.go
@@ -21,7 +21,7 @@ var validate *validator.Validate = validator.New()
 
 var headers = map[string]string{
 	"Access-Control-Allow-Origin":  OriginURL,
-	"Access-Control-Allow-Headers": "Content-Type, x-api-key",
+	"Access-Control-Allow-Headers": "Content-Type, x-amz-content-sha256, x-amz-date, X-Amz-Security-Token, Authorization",
 }
 
 type ImageData struct {
